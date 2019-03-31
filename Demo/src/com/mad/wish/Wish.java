946c057679f84f60790dcc6e18674b8f080aa7ba
TL <lenovo@192.168.0.10>
2019-03-31 23:18:34 +0530

    modi by Dev2

--- Demo/src/com/mad/wish/Wish.java
+++ Demo/src/com/mad/wish/Wish.java
@@ -6,6 +6,7 @@ public class Wish {
 		// TODO Auto-generated method stub
 		System.out.println("hi madhu");  //1234
 		System.out.println("msg from s.o");
+		System.out.println("modi by DEv2");
 
 	}
 
