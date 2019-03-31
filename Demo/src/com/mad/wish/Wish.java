186e522f5b313487a440ed556851c8cb0eac83d9
TL <lenovo@192.168.0.10>
2019-03-31 23:09:43 +0530

    commit by TL

--- Demo/src/com/mad/wish/Wish.java
+++ Demo/src/com/mad/wish/Wish.java
@@ -5,6 +5,7 @@ public class Wish {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi madhu");  //1234
+		System.out.println("msg from s.o");
 
 	}
 
